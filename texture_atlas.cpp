c4cae461d54574fe92d66410590b0e690af0fab5
cuppajoeman <ccn@cuppajoeman.com>
2024-12-08 05:35:02 -0500

    fix: assume non-flipped image

--- texture_atlas.cpp
+++ texture_atlas.cpp
@@ -10,17 +10,22 @@ using json = nlohmann::json;
 TextureAtlas::TextureAtlas(const std::string &json_path, const std::string &image_path, bool flip_texture,
                            bool top_left_coords) {
     using_top_left_coords = top_left_coords;
-    stbi_set_flip_vertically_on_load(1);
+    /*stbi_set_flip_vertically_on_load(1);*/
     load_atlas(json_path, image_path);
 }
 
 void TextureAtlas::load_atlas(const std::string &json_path, const std::string &image_path) {
     std::ifstream file(json_path);
+    if (!file) {
+        std::cerr << "Error: Unable to open JSON file: " << json_path << "\n";
+        return;
+    }
+
     json atlas_json;
     file >> atlas_json;
 
     auto sprites = atlas_json["sub_textures"];
-    for (auto &[key, value] : sprites.items()) {
+    for (auto &[sprite_name, value] : sprites.items()) {
         SpriteInfo info;
         float x = value["x"];
         float y = value["y"];
@@ -28,13 +33,19 @@ void TextureAtlas::load_atlas(const std::string &json_path, const std::string &i
         float height = value["height"];
 
         info.top_left = glm::vec2(x, y);
-        info.bottom_right = glm::vec2(x + width, y + (using_top_left_coords ? -1 : 1) * height);
+        info.bottom_right = glm::vec2(x + width, y + (using_top_left_coords ? 1 : -1) * height);
 
-        sprite_map[key] = info;
+        sprite_map[sprite_name] = info;
+
+        /*// Log the sprite creation*/
+        /*std::cout << "Sprite Created: " << sprite_name << "\n"*/
+        /*          << "  Top-Left: (" << info.top_left.x << ", " << info.top_left.y << ")\n"*/
+        /*          << "  Bottom-Right: (" << info.bottom_right.x << ", " << info.bottom_right.y << ")\n";*/
     }
 
     // Load the texture image
     load_texture(image_path);
+    std::cout << "Texture loaded from: " << image_path << "\n";
 }
 
 void TextureAtlas::load_texture(const std::string &image_path) {
@@ -70,7 +81,15 @@ std::vector<glm::vec2> TextureAtlas::compute_uv_coordinates(const SpriteInfo &sp
 
 std::vector<glm::vec2> TextureAtlas::get_texture_coordinates_of_sub_texture(const std::string &sprite_name) const {
     const SpriteInfo &sprite = sprite_map.at(sprite_name);
-    return compute_uv_coordinates(sprite);
+    auto temp = compute_uv_coordinates(sprite);
+
+    // Log the sprite name and UV coordinates
+    std::cout << "Sprite Name: " << sprite_name << "\n";
+    for (size_t i = 0; i < temp.size(); ++i) {
+        std::cout << "UV[" << i << "]: (" << temp[i].x << ", " << temp[i].y << ")\n";
+    }
+
+    return temp;
 
     /*RenderData data;*/
     /*data.uv_coords = compute_uv_coordinates(sprite);*/
